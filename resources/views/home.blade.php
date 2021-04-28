5619cd362d2be119e507f9c1def2617f97d53d31
Mark Sutherland <mark@marksuth.uk>
2021-04-28 19:55:35 +0100

    tweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('metadescription','Web Designer, Developer and Digital Creative based in Leicester, UK')
 
 @section('page')
-<main>
+<main class="home">
     <h1>Mark Sutherland</h1>
     <p>Web Designer, Developer & Digital Creative</p>
     <p><a href="/contact" class="btn btn-lg btn-light">Contact Me</a></p>
